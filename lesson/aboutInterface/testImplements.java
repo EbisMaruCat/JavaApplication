eee3a24f10c89337375f21413f0b06341da75eec
HasshanEbisCat <hl.kobe.219207725@gmail.com>
2024-06-26 11:11:17 +0900

    Update test

--- lesson/aboutInterface/testImplements.java
+++ lesson/aboutInterface/testImplements.java
@@ -14,7 +14,15 @@ public class testImplements {
                 new Point(1,1)
         );
 
-        testTri.draw();
-        System.out.println(testTri.getArea());
+        t1.draw();
+        System.out.println(t1.getArea());  // 面積:1 ??? 答えが2になっとらん
+
+        Triangle tOwn = new Triangle(
+                new Point(0,2),
+                new Point(0,6),
+                new Point(3,2)
+                // これで3辺の長さが 4,3,5
+        );
+        System.out.println(tOwn.getArea());  // 面積:6
     }
 }
